41b09f1a45d77f2b141b715afa271385dfeca7a2
mRaines08 <mallory.raines@stu.daviess.kyschools.us>
2025-04-16 14:05:52 +0000

    classwork41625

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,25 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		int bottom = 0;
+		int top = ray.length - 1;
+		int middle = top / 2;
+		Arrays.sort(ray);
 
+		System.out.println(Arrays.toString(ray));
+
+		while(!(top < bottom))
+		{
+			if(item > ray[middle])
+				bottom = middle + 1;
+			else if(item < ray[middle])
+				top = middle - 1;
+			else if(ray[middle] == item)
+				return middle;
+
+			middle = (top + bottom) / 2;
+		}
+		
+		return -1;
 	}
 }
